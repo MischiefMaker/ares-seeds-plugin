30909d3f9d57a7c51e523ce50510b7a33fd87a94
MischiefMaker <59433210+MischiefMaker@users.noreply.github.com>
2022-11-24 17:43:36 -0500

    3

--- plugin/commands/NewSeedsCmd.rb
+++ plugin/commands/NewSeedsCmd.rb
@@ -6,25 +6,19 @@ module AresMUSH
       attr_accessor :name, :seed_name, :seed_description
 
       def parse_args
-        args = cmd.parse_args(ArgParser.arg1_equals_arg2_slash_optional_arg3)
-        if (args.arg3 == nil)
-          self.name = enactor_name
-          self.seed_name = titlecase_arg(args.arg1)
-          self.seed_description = trim_arg(args.arg2)
-        else
+        args = cmd.parse_args(ArgParser.arg1_equals_arg2_slash_arg3)
           self.name = titlecase_arg(args.arg1)
           self.seed_name = titlecase_arg(args.arg2)
           self.seed_description = trim_arg(args.arg3)
-        end
       end
 
      def required_args
        [ self.name, self.seed_description, self.seed_name ]
      end
 
-     def check_chargen_locked
-       return nil if Chargen.can_manage_apps?(enactor)
-       Chargen.check_chargen_locked(enactor)
+     def check_can_manage
+      return nil if enactor.has_permission?("view_bgs")
+      return "You're not allowed to edit other people's Seeds."
      end
 
       def handle
@@ -32,7 +26,7 @@ module AresMUSH
         seeds = model.seeds || {}
         seeds[self.seed_name] = self.seed_description
         model.update(seeds: seeds)
-        client.emit_success "Seed set for #{self.name}!"
+        client.emit_success "Seed #{self.seed_name} set for #{self.name}!"
       end
     end
   end
