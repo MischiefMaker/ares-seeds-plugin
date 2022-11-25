d9ccd2f6496a6ba49fa52346fdaf6661c240bfcd
MischiefMaker <59433210+MischiefMaker@users.noreply.github.com>
2022-11-24 20:35:55 -0500

    .

--- plugin/commands/NewSeedsCmd.rb
+++ plugin/commands/NewSeedsCmd.rb
@@ -3,11 +3,11 @@ module AresMUSH
     class NewSeedsCmd
       include CommandHandler
 
-      attr_accessor :name, :seed_name, :seed_description
+      attr_accessor :names, :seed_name, :seed_description
 
       def parse_args
         args = cmd.parse_args(ArgParser.arg1_equals_arg2_slash_arg3)
-          self.name = titlecase_arg(args.arg1)
+          self.names = downcase_arg(args.arg1)
           self.seed_name = titlecase_arg(args.arg2)
           self.seed_description = trim_arg(args.arg3)
       end
@@ -23,13 +23,13 @@ module AresMUSH
 
       def handle
         ClassTargetFinder.with_a_character(self.name, client, enactor) do |model|
-        seeds = model.seeds || {}
-        seeds[self.seed_name] = self.seed_description
-        model.update(seeds: seeds)
-        client.emit_success "Seed #{self.seed_name} set for #{self.name}!"
+          seeds = model.seeds || {}
+          seeds[self.seed_name] = self.seed_description
+          model.update(seeds: seeds)
+          client.emit_success "Seed #{self.seed_name} set for #{self.name}!"
+        end
       end
+      
     end
   end
-end
-
 end
